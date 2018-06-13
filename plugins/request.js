7c72fa51cf2ea5de1ec995907c023014b2e36af7
stobita <tobita.pc@gmail.com>
2018-06-13 23:33:10 +0900

    fix npm

--- plugins/request.js
+++ plugins/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-  baseURL: 'http://localhost:8085'
+  baseURL: 'http://docker-practice-s.tk:8085'
 });
 
 export const setToken = token => {
